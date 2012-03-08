64dd1a395d5026aff224a8afffaf226dd48cca38
Daniel Nordmark <nordmark.daniel@gmail.com>
2012-03-08 20:24:36 +0100

    First pagetype added

---
+++ BrickPileTest/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BrickPile.Domain.Models;
+using BrickPileTest.Models;
+using Raven.Client;
+
+namespace BrickPileTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly Home _model;
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View(_model);
+        }
+
+        public HomeController(IPageModel model, IDocumentSession session)
+        {
+            _model = model as Home;
+        }
+
+    }
+}
---
+++ BrickPileTest/Models/Home.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using BrickPile.Domain.Models;
+using BrickPile.Domain;
+using BrickPileTest.Controllers;
+
+namespace BrickPileTest.Models
+{
+    [PageModel(Name = "Start page", ControllerType = typeof(HomeController))]
+    public class Home : PageModel
+    {
+        [Display(Name = "Main heading")]
+        public string Heading { get; set; }
+        [DataType(DataType.Html)]
+        [Display(Name = "Hero text")]
+        public string HeroText { get; set; }
+    }
+}
